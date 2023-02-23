4083a80f5180c64ccf81a5251360d9e0ea63c502
Newtayo <akandeabdul@gmail.com>
2023-02-23 05:04:36 +0100

    modify linters.yml file

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,14 @@ module.exports = {
   },
  
   devServer: {
-    static: './dist'
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
   },
 
   module: {

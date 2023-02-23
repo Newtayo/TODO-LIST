5122c9f8f9ce4cf41df4b7a0d069216e0a03609e
Newtayo <akandeabdul@gmail.com>
2023-02-23 06:11:41 +0100

    fix lighthouse linter error

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: { bundle: path.resolve(__dirname, 'src/index.js') },
+  entry: { index: path.resolve(__dirname, 'src/index.js') },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js',
